9aa12ac7ee8de7ce24445e4ffd99e32ab4f99b91
kroegerama <chris@kroegerama.com>
2018-06-03 19:07:15 +0200

    changed module name to avoid name collisions px annotation for DimenUtils

--- library/src/main/java/com/kroegerama/kaiteki/DimenUtils.kt
+++ library/src/main/java/com/kroegerama/kaiteki/DimenUtils.kt
@@ -1,8 +1,13 @@
 package com.kroegerama.kaiteki
 
 import android.content.res.Resources
+import android.support.annotation.Px
 import android.util.TypedValue
 
+@Px
 fun Float.dpToPx() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics).toInt()
+
+@Px
 fun Float.spToPx() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics).toInt()
+
 fun Float.spToDp() = this.spToPx() / Resources.getSystem().displayMetrics.scaledDensity;

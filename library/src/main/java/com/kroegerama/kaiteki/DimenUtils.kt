7b347a113065b36c617c41057b97225ca39bb2b3
kroegerama <chris@kroegerama.com>
2018-10-06 23:23:54 +0200

    library update, sdk version 28, kotlin 1.2.71, DimenUtils extended

--- library/src/main/java/com/kroegerama/kaiteki/DimenUtils.kt
+++ library/src/main/java/com/kroegerama/kaiteki/DimenUtils.kt
@@ -1,8 +1,11 @@
 package com.kroegerama.kaiteki
 
+import android.content.Context
 import android.content.res.Resources
+import android.support.annotation.DimenRes
 import android.support.annotation.Px
 import android.util.TypedValue
+import com.kroegerama.kaiteki.baseui.BaseFragment
 
 @Px
 fun Float.dpToPx() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, Resources.getSystem().displayMetrics).toInt()
@@ -10,4 +13,23 @@ fun Float.dpToPx() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this
 @Px
 fun Float.spToPx() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this, Resources.getSystem().displayMetrics).toInt()
 
-fun Float.spToDp() = this.spToPx() / Resources.getSystem().displayMetrics.scaledDensity;
+fun Float.spToDp() = this.spToPx() / Resources.getSystem().displayMetrics.scaledDensity
+
+/** @see Resources.getDimension */
+fun Context.getDimension(@DimenRes res: Int) = resources.getDimension(res)
+
+/** @see Resources.getDimensionPixelSize */
+fun Context.getDimensionPixelSize(@DimenRes res: Int) = resources.getDimensionPixelSize(res)
+
+/** @see Resources.getDimensionPixelOffset */
+fun Context.getDimensionPixelOffset(@DimenRes res: Int) = resources.getDimensionPixelOffset(res)
+
+/** @see Resources.getDimension */
+fun BaseFragment.getDimension(@DimenRes res: Int) = requireContext().getDimension(res)
+
+/** @see Resources.getDimensionPixelSize */
+fun BaseFragment.getDimensionPixelSize(@DimenRes res: Int) = requireContext().getDimensionPixelSize(res)
+
+/** @see Resources.getDimensionPixelOffset */
+fun BaseFragment.getDimensionPixelOffset(@DimenRes res: Int) = requireContext().getDimensionPixelOffset(res)
+

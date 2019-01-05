6ad416e40cc60a8b39f9887b765d33fc91519dc7
kroegerama <chris@kroegerama.com>
2019-01-05 21:24:17 +0100

    add PermissionHandler and LiveDataUtils

---
+++ library/src/main/java/com/kroegerama/kaiteki/PermissionHandler.kt
@@ -0,0 +1,55 @@
+package com.kroegerama.kaiteki
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionHandler(
+    private val context: Activity,
+    private val permission: String,
+    private val requestKey: Int = System.identityHashCode(permission) and 0xffff,
+    private val onGranted: () -> Unit,
+    private val onNotGranted: (() -> Unit)? = null
+) {
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
+                request()
+            } else {
+                onNotGranted?.invoke()
+            }
+        } else {
+            onGranted.invoke()
+        }
+    }
+
+    fun onResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray): Boolean {
+        return when (requestCode) {
+            requestKey -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    onGranted.invoke()
+                } else {
+                    onNotGranted?.invoke()
+                }
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    private fun request() {
+        ActivityCompat.requestPermissions(
+            context,
+            arrayOf(permission),
+            requestKey
+        )
+    }
+}
---
+++ library/src/main/java/com/kroegerama/kaiteki/architecture/LiveDataUtils.kt
@@ -0,0 +1,9 @@
+package com.kroegerama.kaiteki.architecture
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+fun <T> LiveData<T>.observeNonNull(owner: LifecycleOwner, observer: (t: T) -> Unit) {
+    this.observe(owner, Observer { it?.let(observer) })
+}

d38984fc301bcb8cf64edfda03325231b39f7a10
kroegerama <1519044+kroegerama@users.noreply.github.com>
2019-05-25 21:47:15 +0200

    cleanup: move recyclerview package to new library 'recyclerview-kaiteki', remove deprecated FragmentHandler

--- library/src/main/java/com/kroegerama/kaiteki/recyclerview/adapter/AdapterEvents.kt
+++
@@ -1,9 +0,0 @@
-package com.kroegerama.kaiteki.recyclerview.adapter
-
-import android.view.View
-
-interface AdapterEvents<in T> {
-    fun onItemClick(item: T, view: View, position: Int)
-
-    fun onItemLongClick(item: T, view: View, position: Int): Boolean
-}
--- library/src/main/java/com/kroegerama/kaiteki/recyclerview/adapter/ItemProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.kroegerama.kaiteki.recyclerview.adapter
-
-interface ItemProvider<out T> {
-    fun count(): Int
-
-    operator fun get(position: Int): T
-}

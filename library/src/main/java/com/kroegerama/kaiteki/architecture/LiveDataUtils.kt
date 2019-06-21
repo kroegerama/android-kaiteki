ed74cf73333ed2975c90070afd8503427b6c7dc2
kroegerama <1519044+kroegerama@users.noreply.github.com>
2019-06-21 14:03:09 +0200

    libraries updated (kotlin 1.3.40), add LiveData.autoClose()

--- library/src/main/java/com/kroegerama/kaiteki/architecture/LiveDataUtils.kt
+++ library/src/main/java/com/kroegerama/kaiteki/architecture/LiveDataUtils.kt
@@ -2,8 +2,20 @@ package com.kroegerama.kaiteki.architecture
 
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
+import java.io.Closeable
 
 fun <T> LiveData<T>.observeNonNull(owner: LifecycleOwner, observer: (t: T) -> Unit) {
     this.observe(owner, Observer { it?.let(observer) })
+}
+
+/**
+ * Automatically calls current value's .close() before setting a new value
+ */
+fun <T : Closeable> LiveData<T>.autoClose(): LiveData<T> = MediatorLiveData<T>().also { mediator ->
+    mediator.addSource(this) { newValue ->
+        mediator.value?.close()
+        mediator.value = newValue
+    }
 }

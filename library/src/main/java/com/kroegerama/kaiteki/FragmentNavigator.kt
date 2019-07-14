d3efc9a473c52f7dfe6e0a09d88351334c02f259
kroegerama <1519044+kroegerama@users.noreply.github.com>
2019-07-14 21:25:32 +0200

    improve FragmentNavigator

--- library/src/main/java/com/kroegerama/kaiteki/FragmentNavigator.kt
+++ library/src/main/java/com/kroegerama/kaiteki/FragmentNavigator.kt
@@ -8,36 +8,36 @@ import androidx.fragment.app.FragmentTransaction
 import com.kroegerama.kaiteki.baseui.BaseFragment
 import java.lang.ref.WeakReference
 
-class FragmentNavigator(
+class FragmentNavigator<Index>(
         manager: FragmentManager,
-        provider: FragmentProvider,
-        private val strategy: FragmentStrategy = FragmentStrategy.ReplaceStrategy()
+        provider: FragmentProvider<Index>,
+        private val strategy: FragmentStrategy<Index> = FragmentStrategy.ReplaceStrategy()
 ) {
 
-    interface FragmentProvider {
+    interface FragmentProvider<in Index> {
 
         @get:IdRes
         val fragmentContainer: Int
 
-        fun createFragment(index: Int, payload: Any?): Fragment
+        fun createFragment(index: Index, payload: Any?): Fragment
 
-        fun decorateTransaction(fromIndex: Int, toIndex: Int, fragment: Fragment, transaction: FragmentTransaction) {}
+        fun decorateTransaction(fromIndex: Index?, toIndex: Index, fragment: Fragment, transaction: FragmentTransaction) {}
 
-        fun onFragmentSelected(index: Int, fragment: Fragment) {}
+        fun onFragmentSelected(index: Index, fragment: Fragment) {}
 
     }
 
     private val weakManager = WeakReference(manager)
     private val weakProvider = WeakReference(provider)
 
-    private var currentIndex = INDEX_UNKNOWN
+    private var currentIndex: Index? = null
 
-    private fun showInternal(newIndex: Int, fragment: Fragment?, payload: Any?, forceCreate: Boolean): Boolean = synchronized(this) {
+    private fun showInternal(newIndex: Index, fragment: Fragment?, payload: Any?, forceCreate: Boolean): Boolean = synchronized(this) {
         val manager = weakManager.get() ?: return@synchronized false
         val provider = weakProvider.get() ?: return@synchronized false
 
         val oldIndex = currentIndex
-        if (!strategy.accept(oldIndex, newIndex)) return@synchronized false
+        if (oldIndex != null && !strategy.accept(oldIndex, newIndex)) return@synchronized false
 
         val tag = getTagForIndex(newIndex)
         val oldFrag = getCurrentFragment()
@@ -50,11 +50,14 @@ class FragmentNavigator(
             else -> provider.createFragment(newIndex, payload)
         }
         manager.beginTransaction().apply {
-            val oldInfo = oldFrag?.let { FragmentStrategy.FragmentInfo(it, getTagForIndex(oldIndex)) }
+            val oldInfo = oldFrag?.let {
+                val idx = oldIndex ?: return@let null
+                FragmentStrategy.FragmentInfo(it, getTagForIndex(idx))
+            }
             val newInfo = FragmentStrategy.FragmentInfo(newFrag, tag)
 
             (oldFrag as? BaseFragment)?.decorateTransaction(this)
-            strategy.handleTransaction(manager, provider, this, oldInfo, newInfo, newFrag !== previousInstance)
+            strategy.handleTransaction(manager, provider, this, oldInfo, newInfo, newFrag !== previousInstance, forceCreate)
             provider.decorateTransaction(oldIndex, newIndex, newFrag, this)
         }.commitNow()
         provider.onFragmentSelected(newIndex, newFrag)
@@ -62,83 +65,85 @@ class FragmentNavigator(
         true
     }
 
-    fun show(index: Int, payload: Any? = null, forceCreate: Boolean = payload != null) = showInternal(index, null, payload, forceCreate)
+    fun show(index: Index, payload: Any? = null, forceCreate: Boolean = payload != null) = showInternal(index, null, payload, forceCreate)
 
-    fun showFragment(index: Int, fragment: Fragment) = showInternal(index, fragment, null, false)
+    fun showFragment(index: Index, fragment: Fragment) = showInternal(index, fragment, null, false)
 
     fun getCurrentFragment(): Fragment? {
         val manager = weakManager.get() ?: return null
-        return manager.findFragmentByTag(getTagForIndex(currentIndex))
+        val idx = currentIndex ?: return null
+        return manager.findFragmentByTag(getTagForIndex(idx))
     }
 
-    val selection: Int get() = currentIndex
-    val hasSelection: Boolean get() = currentIndex != INDEX_UNKNOWN
+    val selection: Index? get() = currentIndex
+    val hasSelection: Boolean get() = currentIndex != null
 
-    private fun getTagForIndex(index: Int) = "${this.javaClass.simpleName}_frag_$index"
+    private fun getTagForIndex(index: Index) = "${this.javaClass.simpleName}_frag_$index"
 
     fun handleBackPress(): Boolean {
         val current = (getCurrentFragment() ?: return false) as? BaseFragment ?: return false
         return current.handleBackPress()
     }
 
-    fun saveState(outState: Bundle) {
+    fun saveState(outState: Bundle, indexSaver: (index: Index, key: String, bundle: Bundle) -> Unit) {
         outState.putBundle(STATE_BUNDLE, bundle {
-            putInt(STATE_SELECTED, currentIndex)
             putBundle(STATE_STRATEGY, Bundle().apply(strategy::saveState))
+            indexSaver(currentIndex ?: return@bundle, STATE_SELECTED, this)
         })
     }
 
-    fun loadState(bundle: Bundle) {
+    fun loadState(bundle: Bundle, indexLoader: (key: String, bundle: Bundle) -> Index?) {
         val state = bundle.getBundle(STATE_BUNDLE) ?: return
-        currentIndex = state.getInt(STATE_SELECTED, currentIndex)
-        val strategyBundle = state.getBundle(STATE_STRATEGY) ?: return
-        strategy.loadState(strategyBundle)
+        state.getBundle(STATE_STRATEGY)?.let(strategy::loadState)
+        if (state.containsKey(STATE_SELECTED)) {
+            currentIndex = indexLoader(STATE_SELECTED, state)
+        }
     }
 
     companion object {
         private const val STATE_BUNDLE = "FragmentProvider"
         private const val STATE_SELECTED = "selected"
         private const val STATE_STRATEGY = "strategy"
-
-        const val INDEX_UNKNOWN = Int.MIN_VALUE
     }
 }
 
-abstract class FragmentStrategy {
+abstract class FragmentStrategy<Index> {
     data class FragmentInfo(
             val fragment: Fragment,
             val tag: String
     )
 
-    internal abstract fun accept(fromIndex: Int, toIndex: Int): Boolean
-    internal abstract fun mustCreate(index: Int): Boolean
-    internal abstract fun usePreviousInstance(index: Int, instance: Fragment): Boolean
-    internal abstract fun handleTransaction(
+    abstract fun accept(fromIndex: Index, toIndex: Index): Boolean
+    abstract fun mustCreate(index: Index): Boolean
+    abstract fun usePreviousInstance(index: Index, instance: Fragment): Boolean
+    abstract fun handleTransaction(
             manager: FragmentManager,
-            provider: FragmentNavigator.FragmentProvider,
+            provider: FragmentNavigator.FragmentProvider<Index>,
             transaction: FragmentTransaction,
             oldInfo: FragmentInfo?,
             newInfo: FragmentInfo,
-            isNew: Boolean
+            isNew: Boolean,
+            isForced: Boolean
     )
 
-    internal open fun saveState(outState: Bundle) {}
-    internal open fun loadState(bundle: Bundle) {}
+    open fun saveState(outState: Bundle) {}
+    open fun loadState(bundle: Bundle) {}
 
-    class ReplaceStrategy : FragmentStrategy() {
+    open class ReplaceStrategy<Index> : FragmentStrategy<Index>() {
         private val states = HashMap<String, Fragment.SavedState>()
 
-        override fun accept(fromIndex: Int, toIndex: Int) = fromIndex != toIndex
-        override fun mustCreate(index: Int) = true
-        override fun usePreviousInstance(index: Int, instance: Fragment) = false
+        override fun accept(fromIndex: Index, toIndex: Index) = fromIndex != toIndex
+        override fun mustCreate(index: Index) = true
+        override fun usePreviousInstance(index: Index, instance: Fragment) = false
 
         override fun handleTransaction(
                 manager: FragmentManager,
-                provider: FragmentNavigator.FragmentProvider,
+                provider: FragmentNavigator.FragmentProvider<Index>,
                 transaction: FragmentTransaction,
                 oldInfo: FragmentInfo?,
                 newInfo: FragmentInfo,
-                isNew: Boolean
+                isNew: Boolean,
+                isForced: Boolean
         ): Unit = transaction.run {
             if (oldInfo != null) {
                 val (oldFrag, oldTag) = oldInfo
@@ -147,26 +152,50 @@ abstract class FragmentStrategy {
                 remove(oldFrag)
             }
             val (newFrag, newTag) = newInfo
-            val state = states[newTag]
-            if (state != null) {
+            val state = states.remove(newTag)
+            if (!isForced && state != null) {
                 newFrag.setInitialSavedState(state)
             }
             add(provider.fragmentContainer, newFrag, newTag)
         }
+
+        override fun loadState(bundle: Bundle) {
+            states.clear()
+            val keys = bundle.getStringArray(STATE_KEYS).orEmpty()
+            keys.forEach { k ->
+                val v: Fragment.SavedState =
+                        bundle.getParcelable("$STATE_VALUE_PREFIX.$k") ?: return@forEach
+                states[k] = v
+            }
+        }
+
+        override fun saveState(outState: Bundle) {
+            outState.putStringArray(STATE_KEYS, states.keys.toTypedArray())
+            states.entries.forEach {
+                val (k, v) = it
+                outState.putParcelable("$STATE_VALUE_PREFIX.$k", v)
+            }
+        }
+
+        companion object {
+            private const val STATE_KEYS = "state_keys"
+            private const val STATE_VALUE_PREFIX = "sate_value"
+        }
     }
 
-    object DetachStrategy : FragmentStrategy() {
-        override fun accept(fromIndex: Int, toIndex: Int) = fromIndex != toIndex
-        override fun mustCreate(index: Int) = false
-        override fun usePreviousInstance(index: Int, instance: Fragment) = true
+    open class DetachStrategy<Index> : FragmentStrategy<Index>() {
+        override fun accept(fromIndex: Index, toIndex: Index) = fromIndex != toIndex
+        override fun mustCreate(index: Index) = false
+        override fun usePreviousInstance(index: Index, instance: Fragment) = true
 
         override fun handleTransaction(
                 manager: FragmentManager,
-                provider: FragmentNavigator.FragmentProvider,
+                provider: FragmentNavigator.FragmentProvider<Index>,
                 transaction: FragmentTransaction,
                 oldInfo: FragmentInfo?,
                 newInfo: FragmentInfo,
-                isNew: Boolean
+                isNew: Boolean,
+                isForced: Boolean
         ): Unit = transaction.run {
             oldInfo?.fragment?.run(::detach)
             val (newFrag, newTag) = newInfo
@@ -177,18 +206,19 @@ abstract class FragmentStrategy {
         }
     }
 
-    object HideStrategy : FragmentStrategy() {
-        override fun accept(fromIndex: Int, toIndex: Int) = fromIndex != toIndex
-        override fun mustCreate(index: Int) = false
-        override fun usePreviousInstance(index: Int, instance: Fragment) = true
+    open class HideStrategy<Index> : FragmentStrategy<Index>() {
+        override fun accept(fromIndex: Index, toIndex: Index) = fromIndex != toIndex
+        override fun mustCreate(index: Index) = false
+        override fun usePreviousInstance(index: Index, instance: Fragment) = true
 
         override fun handleTransaction(
                 manager: FragmentManager,
-                provider: FragmentNavigator.FragmentProvider,
+                provider: FragmentNavigator.FragmentProvider<Index>,
                 transaction: FragmentTransaction,
                 oldInfo: FragmentInfo?,
                 newInfo: FragmentInfo,
-                isNew: Boolean
+                isNew: Boolean,
+                isForced: Boolean
         ): Unit = transaction.run {
             oldInfo?.fragment?.run(::hide)
             val (newFrag, newTag) = newInfo

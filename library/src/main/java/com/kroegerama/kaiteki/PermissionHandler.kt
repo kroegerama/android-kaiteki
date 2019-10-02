2bb43d4eb7a75037cea532c7572c88aa48f9e10f
kroegerama <1519044+kroegerama@users.noreply.github.com>
2019-10-02 14:35:14 +0200

    fix PermissionHandler

--- library/src/main/java/com/kroegerama/kaiteki/PermissionHandler.kt
+++ library/src/main/java/com/kroegerama/kaiteki/PermissionHandler.kt
@@ -6,49 +6,46 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 class PermissionHandler(
-        private val context: Activity,
-        private val permission: String,
-        private val requestKey: Int = System.identityHashCode(permission) and 0xffff,
-        private val onGranted: () -> Unit,
-        private val onNotGranted: (() -> Unit)? = null
+    private val context: Activity,
+    private val permission: String,
+    private val requestKey: Int = System.identityHashCode(permission) and 0xffff,
+    private val showRationale: () -> Unit,
+    private val onGranted: () -> Unit,
+    private val onNotGranted: () -> Unit = showRationale
 ) {
-    fun checkPermission() {
-        if (ContextCompat.checkSelfPermission(
-                        context,
-                        permission
-                ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
-                request()
-            } else {
-                onNotGranted?.invoke()
-            }
+    fun checkPermission(): Unit = if (ContextCompat.checkSelfPermission(
+            context,
+            permission
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
+            showRationale()
         } else {
-            onGranted.invoke()
+            request()
         }
+    } else {
+        onGranted.invoke()
     }
 
-    fun onResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray): Boolean {
-        return when (requestCode) {
-            requestKey -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    onGranted.invoke()
-                } else {
-                    onNotGranted?.invoke()
-                }
-                true
+    fun onResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray): Boolean = when (requestCode) {
+        requestKey -> {
+            if (grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                onGranted.invoke()
+            } else {
+                onNotGranted.invoke()
             }
-            else -> {
-                false
-            }
+            true
+        }
+        else -> {
+            false
         }
     }
 
     private fun request() {
         ActivityCompat.requestPermissions(
-                context,
-                arrayOf(permission),
-                requestKey
+            context,
+            arrayOf(permission),
+            requestKey
         )
     }
 }

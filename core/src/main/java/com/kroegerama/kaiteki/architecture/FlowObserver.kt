385450c363b13d58679fd5b30fdd8ce5013439ee
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-05-10 20:59:05 +0200

    add Flow.collectLatestWhenStarted

--- core/src/main/java/com/kroegerama/kaiteki/architecture/FlowObserver.kt
+++ core/src/main/java/com/kroegerama/kaiteki/architecture/FlowObserver.kt
@@ -1,11 +1,14 @@
 package com.kroegerama.kaiteki.architecture
 
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.addRepeatingJob
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @PublishedApi
@@ -40,3 +43,10 @@ inline fun <reified T> Flow<T>.observe(
 inline fun <reified T> Flow<T>.observeIn(
     lifecycleOwner: LifecycleOwner
 ) = lifecycleOwner.lifecycle.addObserver(FlowObserver(this))
+
+fun <T> Flow<T>.collectLatestWhenStarted(
+    lifecycleOwner: LifecycleOwner,
+    block: suspend (T) -> Unit
+) = lifecycleOwner.addRepeatingJob(Lifecycle.State.STARTED) {
+    collectLatest(block)
+}

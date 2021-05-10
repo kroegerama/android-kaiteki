1e1e81899ccd2d10cf2363d4f2d7cd3951ad8aa7
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-05-10 21:00:29 +0200

    remove SavedStateHandle.enumField -> use SavedStateHandle.field instead (Enums are Serializable)

--- core/src/main/java/com/kroegerama/kaiteki/architecture/SavedStateHandle.kt
+++ core/src/main/java/com/kroegerama/kaiteki/architecture/SavedStateHandle.kt
@@ -9,9 +9,3 @@ fun <T> SavedStateHandle.field(key: String? = null) = object : ReadWriteProperty
     override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) = set(getKey(property), value)
     override fun getValue(thisRef: Any, property: KProperty<*>): T? = get(getKey(property))
 }
-
-inline fun <reified T : Enum<*>> SavedStateHandle.enumField(key: String? = null) = object : ReadWriteProperty<Any, T?> {
-    private fun getKey(property: KProperty<*>) = key ?: property.name
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) = set<Int>(getKey(property), value?.ordinal)
-    override fun getValue(thisRef: Any, property: KProperty<*>): T? = get<Int>(getKey(property))?.let(enumValues<T>()::get)
-}

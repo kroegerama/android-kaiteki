dc4e8baf97e390d7e41d85d03298494945bebd51
kroegerama <1519044+kroegerama@users.noreply.github.com>
2021-05-10 20:59:30 +0200

    add Fragment.navigate extension functions

--- core/src/main/java/com/kroegerama/kaiteki/architecture/NavUtils.kt
+++ core/src/main/java/com/kroegerama/kaiteki/architecture/NavUtils.kt
@@ -1,9 +1,15 @@
 package com.kroegerama.kaiteki.architecture
 
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavDestination
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.findNavController
 
-private val navLabelArgRegex by lazy { "\\{(.+?)\\}".toRegex() }
+private val navLabelArgRegex by lazy { """[{](\S+?)[}]""".toRegex() }
 
 fun NavDestination.labelWithArgs(args: Bundle?): CharSequence? = with(label) {
     if (isNullOrBlank() || args == null || !contains('{') || !contains('}')) return this
@@ -16,3 +22,27 @@ fun NavDestination.labelWithArgs(args: Bundle?): CharSequence? = with(label) {
         }
     }
 }
+
+fun Fragment.navigate(
+    @IdRes resId: Int,
+    args: Bundle? = null,
+    navOptions: NavOptions? = null,
+    navigatorExtras: Navigator.Extras? = null
+) = findNavController().navigate(resId, args, navOptions, navigatorExtras)
+
+fun Fragment.popBackStack() = findNavController().popBackStack()
+
+fun Fragment.popBackStack(
+    @IdRes destinationId: Int,
+    inclusive: Boolean = false
+) = findNavController().popBackStack(destinationId, inclusive)
+
+fun Fragment.navigate(
+    directions: NavDirections,
+    navOptions: NavOptions? = null
+) = findNavController().navigate(directions, navOptions)
+
+fun Fragment.navigate(
+    directions: NavDirections,
+    navigatorExtras: Navigator.Extras
+) = findNavController().navigate(directions, navigatorExtras)

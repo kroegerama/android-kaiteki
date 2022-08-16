ff453475cd8366469bb80d403b2096b043d67d26
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-08-16 22:56:29 +0200

    improve ViewDelegates code

--- core/src/main/kotlin/com/kroegerama/kaiteki/ViewDelegates.kt
+++ core/src/main/kotlin/com/kroegerama/kaiteki/ViewDelegates.kt
@@ -1,45 +1,29 @@
 package com.kroegerama.kaiteki
 
 import android.graphics.drawable.Drawable
-import android.widget.CheckBox
+import android.widget.CompoundButton
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
-import androidx.appcompat.widget.SwitchCompat
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-operator fun TextView.provideDelegate(value: Any, property: KProperty<*>) = object : ReadWriteProperty<Any, CharSequence?> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): CharSequence? = text
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: CharSequence?) {
-        text = value
-    }
+operator fun TextView.provideDelegate(value: Any, property: KProperty<*>): ReadWriteProperty<Any?, CharSequence?> =
+    ViewDelegateProperty(::getText, ::setText)
+
+operator fun ImageView.provideDelegate(value: Any, property: KProperty<*>): ReadWriteProperty<Any?, Drawable?> =
+    ViewDelegateProperty(::getDrawable, ::setImageDrawable)
+
+operator fun CompoundButton.provideDelegate(value: Any, property: KProperty<*>): ReadWriteProperty<Any?, Boolean> =
+    ViewDelegateProperty(::isChecked, ::setChecked)
+
+operator fun ProgressBar.provideDelegate(value: Any, property: KProperty<*>): ReadWriteProperty<Any?, Int> =
+    ViewDelegateProperty(::getProgress, ::setProgress)
+
+internal class ViewDelegateProperty<T>(
+    private val getter: () -> T,
+    private val setter: (T) -> Unit
+) : ReadWriteProperty<Any?, T> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T = getter()
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) = setter(value)
 }
-
-operator fun ImageView.provideDelegate(value: Any, property: KProperty<*>) = object : ReadWriteProperty<Any, Drawable?> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): Drawable? = drawable
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: Drawable?) = setImageDrawable(value)
-}
-
-operator fun CheckBox.provideDelegate(value: Any, property: KProperty<*>) = object : ReadWriteProperty<Any, Boolean> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): Boolean = isChecked
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: Boolean) {
-        isChecked = value
-    }
-}
-
-operator fun SwitchCompat.provideDelegate(value: Any, property: KProperty<*>) = object : ReadWriteProperty<Any, Boolean> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): Boolean = isChecked
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: Boolean) {
-        isChecked = value
-    }
-}
-
-operator fun ProgressBar.provideDelegate(value: Any, property: KProperty<*>) = object : ReadWriteProperty<Any, Int> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): Int = progress
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: Int) {
-        progress = value
-    }
-}
-
-

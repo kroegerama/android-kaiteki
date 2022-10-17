0559dd48c6f6555199935bc87c1129bb5ddc5fd3
kroegerama <1519044+kroegerama@users.noreply.github.com>
2022-10-17 12:15:22 +0200

    add MenuProvider/MenuHost utils to `MenuUtil`

--- core/src/main/kotlin/com/kroegerama/kaiteki/MenuUtil.kt
+++ core/src/main/kotlin/com/kroegerama/kaiteki/MenuUtil.kt
@@ -1,7 +1,56 @@
 package com.kroegerama.kaiteki
 
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.annotation.IdRes
+import androidx.annotation.MenuRes
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 
 operator fun Menu.get(@IdRes idRes: Int): MenuItem? = findItem(idRes)
+
+fun Fragment.findMenuHost(): MenuHost? {
+    return when (this) {
+        is MenuHostOwner -> menuHost
+        is MenuHost -> this
+        else -> parentFragment?.findMenuHost() ?: activity
+    }
+}
+
+fun MenuHost.addMenuProvider(
+    owner: LifecycleOwner,
+    @MenuRes menuRes: Int,
+    prepareMenu: (Menu) -> Unit = {},
+    menuClosed: (Menu) -> Unit = {},
+    menuItemSelected: (MenuItem) -> Boolean
+) = menuProvider(menuRes, prepareMenu, menuClosed, menuItemSelected).also { provider ->
+    addMenuProvider(provider, owner, Lifecycle.State.STARTED)
+}
+
+fun menuProvider(
+    @MenuRes menuRes: Int,
+    prepareMenu: (Menu) -> Unit = {},
+    menuClosed: (Menu) -> Unit = {},
+    menuItemSelected: (MenuItem) -> Boolean
+): MenuProvider = object : MenuProvider {
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(menuRes, menu)
+    }
+
+    override fun onPrepareMenu(menu: Menu) = prepareMenu(menu)
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean = menuItemSelected(menuItem)
+    override fun onMenuClosed(menu: Menu) = menuClosed(menu)
+}
+
+interface MenuHostOwner {
+    val menuHost: MenuHost
+}
+
+interface MenuProviderOwner {
+    val menuProvider: MenuProvider
+}

f69179688617e270c1f369f60484f7e504c6c5d4
kroegerama <1519044+kroegerama@users.noreply.github.com>
2023-03-26 20:42:05 +0200

    [retrofit] * update `RetrofitResource`   * remove `RetrofitResource.Cancelled`   * add optional `staleData` to subclasses

--- retrofit/src/main/kotlin/com/kroegerama/kaiteki/retrofit/RetrofitResource.kt
+++ retrofit/src/main/kotlin/com/kroegerama/kaiteki/retrofit/RetrofitResource.kt
@@ -7,42 +7,66 @@ import kotlin.Error
 
 sealed class RetrofitResource<out TSuccess> {
 
-    object Cancelled : RetrofitResource<Nothing>()
-
-    data class Running(
-        val currentRetry: Int
-    ) : RetrofitResource<Nothing>()
+    data class Running<out T>(
+        val currentRetry: Int,
+        val staleData: T? = null
+    ) : RetrofitResource<T>()
 
     data class Success<out T>(
         val data: T?,
         val rawResponse: Response
     ) : RetrofitResource<T>()
 
-    data class NoSuccess(
+    data class NoSuccess<out T>(
         val code: Int,
         val errorBody: ResponseBody?,
-        val rawResponse: Response
-    ) : RetrofitResource<Nothing>()
+        val rawResponse: Response,
+        val staleData: T? = null
+    ) : RetrofitResource<T>()
 
-    data class Error(
-        val throwable: Throwable
-    ) : RetrofitResource<Nothing>()
+    data class Error<out T>(
+        val throwable: Throwable,
+        val staleData: T? = null
+    ) : RetrofitResource<T>()
 
     val running get() = this is Running
 
     val isSuccess get() = this is Success
     val isNoSuccess get() = this is NoSuccess
-    val isCancelled get() = this === Cancelled
     val isError get() = this is Error
 
+    @Deprecated("Use dataOrNull instead", ReplaceWith("dataOrNull"))
     fun getOrNull(): TSuccess? = (this as? Success)?.data
 
+    val dataOrNull: TSuccess?
+        get() = when (this) {
+            is Running -> staleData
+            is Success -> data
+            is NoSuccess -> staleData
+            is Error -> staleData
+        }
+
     inline fun <E> map(mapFun: (TSuccess?) -> E?): RetrofitResource<E> = when (this) {
-        Cancelled -> Cancelled
         is Error -> Error(throwable)
         is NoSuccess -> NoSuccess(code, errorBody, rawResponse)
         is Running -> Running(currentRetry)
         is Success -> Success(mapFun(data), rawResponse)
+    }
+
+    inline fun onData(
+        always: Boolean = true,
+        block: RetrofitResource<TSuccess>.(
+            data: TSuccess,
+            isStale: Boolean
+        ) -> Unit
+    ): RetrofitResource<TSuccess> {
+        when (this) {
+            is Running -> staleData?.let { block(it, true) }
+            is Success -> data?.let { block(it, false) }
+            is NoSuccess -> if (always) staleData?.let { block(it, true) }
+            is Error -> if (always) staleData?.let { block(it, true) }
+        }
+        return this
     }
 
     inline fun success(block: Success<TSuccess>.() -> Unit): RetrofitResource<TSuccess> {
@@ -52,14 +76,14 @@ sealed class RetrofitResource<out TSuccess> {
         return this
     }
 
-    inline fun noSuccess(block: NoSuccess.() -> Unit): RetrofitResource<TSuccess> {
+    inline fun noSuccess(block: NoSuccess<TSuccess>.() -> Unit): RetrofitResource<TSuccess> {
         if (this is NoSuccess) {
             block(this)
         }
         return this
     }
 
-    inline fun error(block: Error.() -> Unit): RetrofitResource<TSuccess> {
+    inline fun error(block: Error<TSuccess>.() -> Unit): RetrofitResource<TSuccess> {
         if (this is Error) {
             block(this)
         }

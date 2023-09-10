efbeb0b4cb7e6836feaa15eed763f36da5b7897b
kroegerama <1519044+kroegerama@users.noreply.github.com>
2023-09-10 20:53:28 +0200

    cleanup build

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,15 +1,5 @@
-object Plugins {
-    const val nexusPublish = "io.github.gradle-nexus.publish-plugin"
-}
-
 object Android {
     const val minSdk = 21
-    const val compileSdk = 33
-    const val targetSdk = 33
-}
-
-object Libs {
-
-    const val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.5"
-
+    const val compileSdk = 34
+    const val targetSdk = 34
 }

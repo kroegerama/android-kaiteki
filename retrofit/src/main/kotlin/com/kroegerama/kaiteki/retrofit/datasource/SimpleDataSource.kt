26acf29aab998608deba3f1cff87b0a9302b08c4
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-03-03 22:45:39 +0100

    add `SimpleDataSource`

---
+++ retrofit/src/main/kotlin/com/kroegerama/kaiteki/retrofit/datasource/SimpleDataSource.kt
@@ -0,0 +1,37 @@
+package com.kroegerama.kaiteki.retrofit.datasource
+
+import androidx.fragment.app.Fragment
+import com.kroegerama.kaiteki.prepareLaunchWithProgress
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.firstOrNull
+
+data class SimpleDataSource<T>(
+    val flow: SharedFlow<T>,
+    val loading: StateFlow<Boolean>,
+    private val refreshFun: () -> Unit
+) {
+
+    fun refresh() = refreshFun()
+
+    val latest get() = flow.replayCache.lastOrNull()
+
+    companion object {
+
+        fun <T> Fragment.withSimpleDataSourceValue(
+            dataSource: SimpleDataSource<T>,
+            block: (T) -> Unit
+        ) {
+            val cached = dataSource.latest
+            if (cached != null) {
+                block(cached)
+            } else {
+                prepareLaunchWithProgress {
+                    dataSource.flow.firstOrNull()
+                }.startAndThen {
+                    it?.let(block)
+                }
+            }
+        }
+    }
+}

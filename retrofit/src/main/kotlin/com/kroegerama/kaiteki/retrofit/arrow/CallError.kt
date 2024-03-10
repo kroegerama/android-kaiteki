dbb2083730d771c3a3eaea74d0f1ab064f0a5531
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-03-10 18:36:43 +0100

    improve CallError, add `cause`

--- retrofit/src/main/kotlin/com/kroegerama/kaiteki/retrofit/arrow/CallError.kt
+++ retrofit/src/main/kotlin/com/kroegerama/kaiteki/retrofit/arrow/CallError.kt
@@ -6,47 +6,47 @@ import java.io.IOException
 
 sealed interface TypedCallError<out E> {
     val code: Int?
+    val cause: Throwable
 }
 sealed interface CallError : TypedCallError<Nothing>
 
 data class HttpError(
     override val code: Int,
     val message: String,
-    val body: ResponseBody?
+    val body: ResponseBody?,
+    override val cause: Throwable = RuntimeException("HTTP $code")
 ) : CallError
 
 data class TypedHttpError<out E : Any>(
     override val code: Int,
     val message: String,
-    val body: E
+    val body: E,
+    override val cause: Throwable = RuntimeException("HTTP $code")
 ) : TypedCallError<E>
 
 data class IOError(
-    val cause: IOException
+    override val cause: IOException
 ) : CallError {
     override val code: Int? = null
 }
 
 data class UnexpectedError(
-    val cause: Throwable
+    override val cause: Throwable
 ) : CallError {
     override val code: Int? = null
 }
 
-data class ThrowableCallError(
-    val delegate: CallError
-) : Throwable(
-    message = when (delegate) {
+data class TypedCallErrorException(
+    val delegate: TypedCallError<*>
+) : RuntimeException(
+    when (delegate) {
+        is TypedHttpError -> "HTTP ${delegate.code}"
         is HttpError -> "HTTP ${delegate.code}"
-        is IOError -> delegate.cause.message
-        is UnexpectedError -> delegate.cause.message
+        is IOError -> delegate.cause.run { localizedMessage ?: message ?: javaClass.simpleName }
+        is UnexpectedError -> delegate.cause.run { localizedMessage ?: message ?: javaClass.simpleName }
     },
-    cause = when (delegate) {
-        is HttpError -> null
-        is IOError -> delegate.cause
-        is UnexpectedError -> delegate.cause
-    }
+    delegate.cause
 )
 
-fun CallError.throwable() = ThrowableCallError(this)
-fun <Key : Any, Value : Any> CallError.loadResultError() = PagingSource.LoadResult.Error<Key, Value>(throwable())
+fun TypedCallError<*>.exception() = TypedCallErrorException(this)
+fun <Key : Any, Value : Any> TypedCallError<*>.loadResultError() = PagingSource.LoadResult.Error<Key, Value>(exception())

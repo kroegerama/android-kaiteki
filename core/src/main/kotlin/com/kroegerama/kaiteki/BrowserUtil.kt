b69c76a9526be5c629d4123ac892f24ce5471d97
kroegerama <1519044+kroegerama@users.noreply.github.com>
2024-06-16 10:52:51 +0200

    BrowserUtil: add `Fragment.openCustomChromeTab`

--- core/src/main/kotlin/com/kroegerama/kaiteki/BrowserUtil.kt
+++ core/src/main/kotlin/com/kroegerama/kaiteki/BrowserUtil.kt
@@ -6,30 +6,34 @@ import android.graphics.Color
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
-import com.google.android.material.R
+import androidx.fragment.app.Fragment
 import com.google.android.material.color.MaterialColors
+
+private fun CustomTabsIntent.Builder.defaultScheme(context: Context) {
+    setDefaultColorSchemeParams(
+        CustomTabColorSchemeParams.Builder()
+            .setToolbarColor(
+                MaterialColors.getColor(
+                    context,
+                    com.google.android.material.R.attr.colorPrimary,
+                    Color.WHITE
+                )
+            )
+            .setSecondaryToolbarColor(
+                MaterialColors.getColor(
+                    context,
+                    com.google.android.material.R.attr.colorPrimaryVariant,
+                    Color.WHITE
+                )
+            )
+            .build()
+    )
+}
 
 fun Context.openCustomChromeTab(
     uri: Uri,
     block: CustomTabsIntent.Builder.() -> Unit = {
-        setDefaultColorSchemeParams(
-            CustomTabColorSchemeParams.Builder()
-                .setToolbarColor(
-                    MaterialColors.getColor(
-                        this@openCustomChromeTab,
-                        R.attr.colorPrimary,
-                        Color.WHITE
-                    )
-                )
-                .setSecondaryToolbarColor(
-                    MaterialColors.getColor(
-                        this@openCustomChromeTab,
-                        R.attr.colorPrimaryVariant,
-                        Color.WHITE
-                    )
-                )
-                .build()
-        )
+        defaultScheme(this@openCustomChromeTab)
     }
 ): Boolean {
     CustomTabsIntent.Builder()
@@ -55,3 +59,10 @@ fun Context.openCustomChromeTab(
             }
         }
 }
+
+fun Fragment.openCustomChromeTab(
+    uri: Uri,
+    block: CustomTabsIntent.Builder.() -> Unit = {
+        defaultScheme(requireContext())
+    }
+) = requireContext().openCustomChromeTab(uri, block)

c0543acf97701919232729aa0a91dd386b167b77
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-05-07 09:37:48 +0200

    Compose * improve `UnicodeFlagPreview`

--- compose/src/main/kotlin/com/kroegerama/kaiteki/compose/utils/LocaleUtils.kt
+++ compose/src/main/kotlin/com/kroegerama/kaiteki/compose/utils/LocaleUtils.kt
@@ -1,16 +1,21 @@
 package com.kroegerama.kaiteki.compose.utils
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toPersistentList
+import java.text.Collator
 import java.util.Locale
 
 val Locale.unicodeFlag: String
@@ -27,28 +32,31 @@ val Locale.unicodeFlag: String
 @Preview(showBackground = true)
 @Composable
 private fun UnicodeFlagPreview() {
-    Column(
+    val items = remember {
+        val collator = Collator.getInstance()
+        Locale.getISOCountries().map {
+            Locale("", it)
+        }.sortedWith(
+            Comparator.comparing(
+                { it.displayCountry },
+                collator
+            )
+        ).toPersistentList()
+    }
+
+    LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.padding(16.dp)
+        contentPadding = PaddingValues(16.dp),
+        modifier = Modifier.fillMaxSize()
     ) {
-        persistentListOf(
-            Locale.GERMANY,
-            Locale("", "IE"),
-            Locale.UK,
-            Locale.FRANCE,
-            Locale.JAPAN,
-            Locale.ITALY,
-            Locale.US,
-            Locale.CANADA,
-            Locale.KOREA,
-            Locale.TAIWAN,
-            Locale.CHINA,
-            Locale("", "ES"),
-            Locale("", "LK")
-        ).forEach {
+        items(
+            items = items,
+            key = { it }
+        ) {
             Text(
                 text = it.unicodeFlag + "\u2003" + it.displayCountry,
-                fontSize = 24.sp
+                fontSize = 18.sp,
+                modifier = Modifier.heightIn(min = 32.dp)
             )
         }
     }

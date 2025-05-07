fdd77cd14bd839cecac122c4a44654e45965b8da
kroegerama <1519044+kroegerama@users.noreply.github.com>
2025-05-07 09:34:37 +0200

    Compose * move `LocalLocalizedDateTimeFormatterPreview` into `LocalizedDateTimeFormatter`

--- compose/src/main/kotlin/com/kroegerama/kaiteki/compose/formatting/LocalizedDateTimeFormatter.kt
+++ compose/src/main/kotlin/com/kroegerama/kaiteki/compose/formatting/LocalizedDateTimeFormatter.kt
@@ -1,11 +1,22 @@
 package com.kroegerama.kaiteki.compose.formatting
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kroegerama.kaiteki.compose.LocalLocale
+import com.kroegerama.kaiteki.compose.LocalLocalizedDateTimeFormatter
 import com.kroegerama.kaiteki.compose.LocalZoneId
+import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -63,4 +74,39 @@ fun rememberLocalizedDateTimeFormatter(
         locale = locale,
         zoneId = zoneId
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun LocalLocalizedDateTimeFormatterPreview() {
+    val dateTime = remember {
+        OffsetDateTime.now()
+    }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Text(
+            text = "not set"
+        )
+        Text(
+            text = LocalLocalizedDateTimeFormatter.current.formatDateTime(dateTime)
+        )
+        val localFormatter = rememberLocalizedDateTimeFormatter(
+            dateStyle = FormatStyle.LONG,
+            timeStyle = FormatStyle.MEDIUM
+        )
+        HorizontalDivider()
+        CompositionLocalProvider(
+            LocalLocalizedDateTimeFormatter provides localFormatter
+        ) {
+            Text(
+                text = "set"
+            )
+            Text(
+                text = LocalLocalizedDateTimeFormatter.current.formatDateTime(dateTime)
+            )
+        }
+    }
 }
